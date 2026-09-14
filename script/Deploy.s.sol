fc27cc45a08589d56c83df90e53ccdd373721cac
William Morriss <wjmelements@gmail.com>
2026-09-14 17:03:26 -0500

    fix: import path casing

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {stdJson} from "forge-std/StdJson.sol";
 import {VmSafe} from "forge-std/Vm.sol";
 
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/erc1967/ERC1967Proxy.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import {ServiceRewardsActor} from "../src/ServiceRewardsActor.sol";
 import {StreamWeightActor} from "../src/StreamWeightActor.sol";
