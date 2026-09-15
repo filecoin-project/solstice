8e3305bc263e57dcad281ce877d096cba63bd534
William Morriss <wjmelements@gmail.com>
2026-09-15 13:20:10 -0500

    fix: nonpayablePayable

--- src/lib/UnanimousProxied.sol
+++ src/lib/UnanimousProxied.sol
@@ -38,7 +38,26 @@ contract UnanimousProxied is Initializable, UnanimousGovernance, UUPSUpgradeable
         emit OwnerReplaced(prevOwner, newOwner);
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override unanimous(keccak256(msg.data), HOLD) {}
+    error NonzeroCallValue();
+
+    /// @dev Solidity doesn't let you override a payable method as nonpayable.
+    ///      This is a modifier so that it can be applied before `unanimous`
+    modifier nonpayablePayable() {
+        require(msg.value == 0, NonzeroCallValue());
+        _;
+    }
+
+    function upgradeToAndCall(address newImplementation, bytes memory data)
+        public
+        payable
+        override
+        nonpayablePayable
+        unanimous(keccak256(msg.data), HOLD)
+    {
+        super.upgradeToAndCall(newImplementation, data);
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 
     /// @notice An owner cancels a pending unanimous task before it executes.
     /// @param taskId The pending task's identifier, usually keccak256(msg.data) of its submission.
