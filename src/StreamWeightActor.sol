7bfb24fbc0937fc2d6c88553f1b55748e5149fc4
William Morriss <wjmelements@gmail.com>
2026-09-22 03:31:45 -0500

    fix: past tense

--- src/StreamWeightActor.sol
+++ src/StreamWeightActor.sol
@@ -70,7 +70,7 @@ contract StreamWeightActor is UnanimousProxied {
         FVMRewards.removeStream(id);
     }
 
-    event SetWeightRecords(WeightRecordUpdate[] updates);
+    event WeightRecordsSet(WeightRecordUpdate[] updates);
 
     /// @notice Queues a discretionary weight-schedule write for one or more streams.
     /// @param updates Id/record pairs to write.
@@ -78,7 +78,7 @@ contract StreamWeightActor is UnanimousProxied {
         // hold enforced in f02
         FVMRewards.setWeightRecords(updates);
         GateParamsLibrary.getGateCheckSlot().pendingWeightUntil = currentEpoch() + HOLD;
-        emit SetWeightRecords(updates);
+        emit WeightRecordsSet(updates);
     }
 
     /// @notice Queues a writer change for an explicit stream.
@@ -155,14 +155,14 @@ contract StreamWeightActor is UnanimousProxied {
         _setGateParams(params, taskId);
     }
 
-    event SetGateParams(GateParams params);
+    event GateParamsSet(GateParams params);
 
     function _setGateParams(GateParams calldata params, bytes32 taskId) private unanimous(taskId, HOLD) {
         require(params.steps <= GateParamsLibrary.GATE_STEPS, StepsOutOfRange());
         GateParamsLibrary.GateParamsInfo storage gateParamsInfo = GateParamsLibrary.getGateParamsSlot();
         gateParamsInfo.params = params;
         delete GateParamsLibrary.getGateCheckSlot().pendingGateParamsTaskId;
-        emit SetGateParams(params);
+        emit GateParamsSet(params);
     }
 
     function veto(bytes32 taskId) public override {
